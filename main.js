c371aa26de5f1071fe6e92546c84d3bdb1de82bc
Christian Vasquez <christianvasquezmelo@gmail.com>
2018-03-17 08:56:33 -0400

    Replace let for const

--- main.js
+++ main.js
@@ -3,15 +3,15 @@ $(document).ready(function () {
     $('.collapsible').collapsible();
 });
 
-let filterInput = document.getElementById('filterInput');
+const filterInput = document.getElementById('filterInput');
 filterInput.addEventListener('keyup', filterNames);
 
 function filterNames() {
-    let filterValue = document.getElementById('filterInput').value.toUpperCase();
-    let ul = document.getElementById('names');
-    let li = ul.querySelectorAll('li.collection-item');
+    const filterValue = document.getElementById('filterInput').value.toUpperCase();
+    const ul = document.getElementById('names');
+    const li = ul.querySelectorAll('li.collection-item');
     for (let i = 0; i < li.length; i++) {
-        let a = li[i].getElementsByTagName('a')[0];
+        const a = li[i].getElementsByTagName('a')[0];
 
         if (a.innerHTML.toUpperCase().indexOf(filterValue) > -1) {
             li[i].style.display = '';
